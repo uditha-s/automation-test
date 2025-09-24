bc22db980c3795908d7ff2ac554b7b5325cd961f
uditha-s <albauditha@gmail.com>
2025-09-24 23:37:18 +0530

    search test cases added create utill for search funtion

---
+++ tests/e2e/verify_search_and_add_to_cart.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from "../../fixtures/fixtures";
+import "../../utils/randomSearch"
+import searchData from "../../testdata/searchData.json";
+import { getRandomItem } from "../../utils/randomSearch";
+
+
+test.describe("User search items and add to cart ", () => {
+
+  test('verify that user can search items in the website ', async ({ page, navbar, loggedIn,accountPage }) => {
+   const randomSearchItem = getRandomItem(searchData.items);
+   await accountPage.navigateToHome();
+   await navbar.searchForItem(randomSearchItem);
+  });
+   test('verify that user can see error mesaage when search invalid item ', async ({ page, navbar, loggedIn,accountPage }) => {
+   await accountPage.navigateToHome();
+   await navbar.searchForItem("laptop");
+  });
+ test('verify that user can see error mesaage when search with empty field ', async ({ page, navbar, loggedIn,accountPage }) => {
+   await accountPage.navigateToHome();
+   await navbar.searchForItem("laptop");
+  });
+   test('verify that user can add items in to the cart ', async ({ page, navbar, loggedIn,accountPage }) => {
+   await accountPage.navigateToHome();
+   await navbar.searchForItem("laptop");
+  });
+   test('verify that user can remove items in to the cart ', async ({ page, navbar, loggedIn,accountPage }) => {
+   await accountPage.navigateToHome();
+   await navbar.searchForItem("laptop");
+  });
+  
+
+});
